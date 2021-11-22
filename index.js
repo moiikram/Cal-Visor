be5104a8b27c681533fa9bc6b61ea6213816e2b9
Ikram Khan <33215354+moiikram@users.noreply.github.com>
2021-11-22 16:33:01 -0600

    adding the initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+// function myFunction() {
+//   window.alert("Hello world!");
+//   // document.getElementById("demo").innerHTML = "Paragraph changed.";
+// }
+
+// console.log("Hello First");
+// alert("Hello World");
+
+console.log("Hello World!");
